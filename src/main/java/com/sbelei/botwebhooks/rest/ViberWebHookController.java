98f1f1372a47fbb5d7553fcbf2a29a38dcf67253
Sergiy Beley <sergiy.beley@gmail.com>
2021-12-08 10:52:11 +0200

    /viber/receive endpoint handle all kind of requests

--- src/main/java/com/sbelei/botwebhooks/rest/ViberWebHookController.java
+++ src/main/java/com/sbelei/botwebhooks/rest/ViberWebHookController.java
@@ -1,7 +1,6 @@
 package com.sbelei.botwebhooks.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/viber")
 public class ViberWebHookController {
 
-    @GetMapping("/receive")
+    //Viber requires that we handle any request here and always return 200
+    @RequestMapping("/receive")
     public ResponseEntity<String> receive() {
         return ResponseEntity.ok("Hello world");
     }

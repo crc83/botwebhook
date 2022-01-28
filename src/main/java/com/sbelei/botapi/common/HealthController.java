128b06805d8bb567638427783c06ce7647f03b59
Sergiy Beley <sergiy.beley@gmail.com>
2022-01-28 22:05:10 +0200

    Add health endpoint

---
+++ src/main/java/com/sbelei/botapi/common/HealthController.java
@@ -0,0 +1,18 @@
+package com.sbelei.botapi.common;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
+    }
+}
